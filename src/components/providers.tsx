1547d66e9995212d7fca0112033569b0fee67a50
vladmee <vladpresedinte@gmail.com>
2025-08-30 08:47:40 +0800

    change providers order

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -20,19 +20,23 @@ const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <GelatoSmartWalletContextProvider
-      settings={{
-        scw: {
-          type: "gelato", // use gelato, kernel, safe, or custom
-        },
-        apiKey: process.env.NEXT_PUBLIC_GELATO_API_KEY as string,
-        defaultChain: inkSepolia,
-        waas: dynamic(process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID as string),
-        wagmi: wagmiConfig,
-      }}
-    >
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </GelatoSmartWalletContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <GelatoSmartWalletContextProvider
+        settings={{
+          scw: {
+            type: "gelato", // use gelato, kernel, safe, or custom
+          },
+          apiKey: process.env.NEXT_PUBLIC_GELATO_API_KEY as string,
+          defaultChain: inkSepolia,
+          waas: dynamic(
+            process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID as string
+          ),
+          wagmi: wagmiConfig,
+        }}
+      >
+        {children}
+      </GelatoSmartWalletContextProvider>
+    </QueryClientProvider>
   );
 };
 
